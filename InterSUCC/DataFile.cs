7be1f3e9c728b32d564bc37a097ce94e19466d13
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-01-17 19:36:44 -0500

    added generic DataFile

---
+++ InterSUCC/DataFile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using SUCC;
+using SUCC.Abstractions;
+using ClassImpl;
+
+namespace InterSUCC
+{
+    public class DataFile<TData> : DataFile where TData : class
+    {
+        private static TData ImplDataCache;
+        private static PropertyInfo[] DataProperties;
+
+        public TData Data { get; }
+
+
+        public DataFile(string path, string defaultFileText = null, bool autoSave = true, bool autoReload = false) : this(path, FileStyle.Default, defaultFileText, autoSave, autoReload) { }
+
+        public DataFile(string path, FileStyle style, string defaultFileText = null, bool autoSave = true, bool autoReload = false) : base(path, style, defaultFileText, autoSave, autoReload)
+        {
+            if (!typeof(TData).IsInterface)
+                throw new Exception($"{nameof(TData)} in {nameof(DataFile<TData>)} must be an interface type");
+
+            if (ImplDataCache == null)
+            {
+                var impl = new Implementer<TData>(this.GetType());
+                DataProperties = impl.Properties;
+
+                foreach (var prop in DataProperties)
+                {
+                    if (prop.GetMethod != null)
+                    {
+                        impl.Getter(prop).Callback(o =>
+                        {
+                            var file = o["__data"] as DataFile<TData>;
+                            return file.GetNonGeneric(
+                                type: prop.PropertyType,
+                                key: prop.Name,
+                                DefaultValue: Activator.CreateInstance(prop.PropertyType));
+                        });
+                    }
+
+                    if (prop.SetMethod != null)
+                    {
+                        impl.Setter(prop, (value, data) =>
+                        {
+                            var file = (DataFile<TData>)data;
+                            file.SetNonGeneric(
+                                type: prop.PropertyType,
+                                key: prop.Name,
+                                value);
+                        });
+                    }
+                }
+
+                ImplDataCache = impl.Finish();
+            }
+
+            this.Data = ClassUtils.Copy(ImplDataCache, this);
+        }
+    }
+}
