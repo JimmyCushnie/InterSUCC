0d63c48754b1c0fedfb5b3509c45d8b079584019
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-01-17 20:12:11 -0500

    abstracted data generation code

--- InterSUCC/DataFile.cs
+++ InterSUCC/DataFile.cs
@@ -10,9 +10,6 @@ namespace InterSUCC
 {
     public class DataFile<TData> : DataFile where TData : class
     {
-        private static TData ImplDataCache;
-        private static PropertyInfo[] DataProperties;
-
         public TData Data { get; }
 
 
@@ -20,45 +17,7 @@ namespace InterSUCC
 
         public DataFile(string path, FileStyle style, string defaultFileText = null, bool autoSave = true, bool autoReload = false) : base(path, style, defaultFileText, autoSave, autoReload)
         {
-            if (!typeof(TData).IsInterface)
-                throw new Exception($"{nameof(TData)} in {nameof(DataFile<TData>)} must be an interface type");
-
-            if (ImplDataCache == null)
-            {
-                var impl = new Implementer<TData>(this.GetType());
-                DataProperties = impl.Properties;
-
-                foreach (var prop in DataProperties)
-                {
-                    if (prop.GetMethod != null)
-                    {
-                        impl.Getter(prop).Callback(o =>
-                        {
-                            var file = o["__data"] as DataFile<TData>;
-                            return file.GetNonGeneric(
-                                type: prop.PropertyType,
-                                key: prop.Name,
-                                DefaultValue: prop.PropertyType.GetDefaultValue());
-                        });
-                    }
-
-                    if (prop.SetMethod != null)
-                    {
-                        impl.Setter(prop, (value, data) =>
-                        {
-                            var file = (DataFile<TData>)data;
-                            file.SetNonGeneric(
-                                type: prop.PropertyType,
-                                key: prop.Name,
-                                value);
-                        });
-                    }
-                }
-
-                ImplDataCache = impl.Finish();
-            }
-
-            this.Data = ClassUtils.Copy(ImplDataCache, this);
+            this.Data = DataUtility<TData>.GenerateDataObject(this);
         }
     }
 }
---
+++ InterSUCC/DataUtility.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using SUCC;
+using SUCC.Abstractions;
+using ClassImpl;
+
+namespace InterSUCC
+{
+    internal static class DataUtility<TData>
+    {
+        private static TData ImplDataCache;
+        private static PropertyInfo[] DataProperties;
+
+        internal static TData GenerateDataObject(object dataObject)
+        {
+            if (!typeof(TData).IsInterface)
+                throw new Exception($"{nameof(TData)} must be an interface type");
+
+            if (ImplDataCache == null)
+            {
+                var impl = new Implementer<TData>(dataObject.GetType());
+                DataProperties = impl.Properties;
+
+                foreach (var prop in DataProperties)
+                {
+                    if (prop.GetMethod != null)
+                    {
+                        impl.Getter(prop).Callback(o =>
+                        {
+                            var file = o["__data"] as ReadableDataFile;
+
+                            return file.GetNonGeneric(
+                                type: prop.PropertyType,
+                                key: prop.Name,
+                                defaultValue: prop.PropertyType.GetDefaultValue());
+                        });
+                    }
+
+                    if (prop.SetMethod != null)
+                    {
+                        impl.Setter(prop, (value, data) =>
+                        {
+                            var file = (ReadableWritableDataFile)data;
+                            file?.SetNonGeneric(
+                                type: prop.PropertyType,
+                                key: prop.Name,
+                                value);
+                        });
+                    }
+                }
+
+                ImplDataCache = impl.Finish();
+            }
+
+            return ClassUtils.Copy(ImplDataCache, dataObject);
+        }
+    }
+}
