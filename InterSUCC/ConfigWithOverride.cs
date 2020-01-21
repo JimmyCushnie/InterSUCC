46fb782718a363903145092fea57d03cacbbf391
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-01-21 15:53:59 -0500

    separate data types for base and override files

--- InterSUCC/ConfigWithOverride.cs
+++ InterSUCC/ConfigWithOverride.cs
@@ -9,19 +9,22 @@ using ClassImpl;
 
 namespace InterSUCC
 {
-    public class ConfigWithOverride<TData> where TData : class
+    public class ConfigWithOverride<TBaseData, TOverrideData>
+        where TOverrideData : class
+        where TBaseData : class, TOverrideData
     {
-        private DataFile<TData> BaseFile { get; }
-        private DataFile<TData> OverrideFile { get; set; }
+        private DataFile<TBaseData> BaseFile { get; }
+        private DataFile<TOverrideData> OverrideFile { get; set; }
         private string OverrideFilesDefaultText { get; }
 
         public string FileName { get; }
-        public TData Data { get; }
-        public TData Override => OverrideFile?.Data;
+        public TOverrideData Data { get; }
+
+        public TOverrideData Override => OverrideFile?.Data;
 
         public ConfigWithOverride(string basePath, string baseFileDefaultText = null, string overrideFilesDefaultText = null)
         {
-            BaseFile = new DataFile<TData>(basePath, defaultFileText: baseFileDefaultText);
+            BaseFile = new DataFile<TBaseData>(basePath, defaultFileText: baseFileDefaultText);
             this.FileName = BaseFile.FileName;
             this.OverrideFilesDefaultText = overrideFilesDefaultText;
 
@@ -31,19 +34,19 @@ namespace InterSUCC
         public void SetOverrideDirectory(string directory)
         {
             string path = Path.Combine(directory, FileName + "_override");
-            OverrideFile = new DataFile<TData>(path, defaultFileText: OverrideFilesDefaultText);
+            OverrideFile = new DataFile<TOverrideData>(path, defaultFileText: OverrideFilesDefaultText);
         }
 
 
-        private static TData ImplDataCache;
-        private TData GenerateDataObject()
+        private static TOverrideData ImplDataCache;
+        private TOverrideData GenerateDataObject()
         {
-            if (!typeof(TData).IsInterface)
-                throw new Exception($"{nameof(TData)} must be an interface type");
+            if (!typeof(TOverrideData).IsInterface)
+                throw new Exception($"{nameof(TOverrideData)} must be an interface type");
 
             if (ImplDataCache == null)
             {
-                var impl = new Implementer<TData>(this.GetType());
+                var impl = new Implementer<TOverrideData>(this.GetType());
 
                 foreach (var prop in impl.Properties)
                 {
@@ -51,18 +54,22 @@ namespace InterSUCC
                     {
                         impl.Getter(prop).Callback(o =>
                         {
-                            var files = o["__data"] as ConfigWithOverride<TData>;
-                            DataFile<TData> appropriateFile;
+                            var files = o["__data"] as ConfigWithOverride<TBaseData, TOverrideData>;
 
                             if (files.OverrideFile.KeyExists(prop.Name))
-                                appropriateFile = files.OverrideFile;
+                            {
+                                return files.OverrideFile.GetNonGeneric(
+                                    type: prop.PropertyType,
+                                    key: prop.Name,
+                                    defaultValue: prop.PropertyType.GetDefaultValue());
+                            }
                             else
-                                appropriateFile = files.BaseFile;
-
-                            return appropriateFile.GetNonGeneric(
-                                type: prop.PropertyType,
-                                key: prop.Name,
-                                defaultValue: prop.PropertyType.GetDefaultValue());
+                            {
+                                return files.BaseFile.GetNonGeneric(
+                                    type: prop.PropertyType,
+                                    key: prop.Name,
+                                    defaultValue: prop.PropertyType.GetDefaultValue());
+                            }
                         });
                     }
 
@@ -70,7 +77,7 @@ namespace InterSUCC
                     {
                         impl.Setter(prop, (value, data) =>
                         {
-                            var files = (ConfigWithOverride<TData>)data;
+                            var files = (ConfigWithOverride<TBaseData, TOverrideData>)data;
                             files.BaseFile.SetNonGeneric(
                                 type: prop.PropertyType,
                                 key: prop.Name,
