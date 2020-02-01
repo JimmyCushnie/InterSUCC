a9e3759c6bb633c85e6061f90447122a3c19e8f6
Jimmy Cushnie <jimmy@mousehatgames.com>
2020-01-31 21:26:55 -0500

    added WithDefaultFile methods to generic DataFile types

--- InterSUCC/DataFile types/DataFileGeneric.cs
+++ InterSUCC/DataFile types/DataFileGeneric.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using SUCC;
+using SUCC.UnityStuff;
 using SUCC.Abstractions;
 using ClassImpl;
 
@@ -15,6 +16,19 @@ namespace InterSUCC
         public DataFile(string path, string defaultFileText = null) : base(path, defaultFileText)
         {
             this.Data = DataUtility<TData>.GenerateDataObject(this);
+        }
+
+
+        /// <summary>
+        /// Creates a new <see cref="DataFile{TData}"/> object, using a text file in Unity's Resources folder for the default file text.
+        /// </summary>
+        /// <param name="path"> The path of the file. Can be either absolute or relative to the default path. </param>
+        /// <param name="defaultFile"> The path of the default file, relative to any Resources parent. </param>
+        /// <returns></returns>
+        public static new DataFile<TData> WithDefaultFile(string path, string defaultFile)
+        {
+            string defaultFileText = ResourcesUtilities.ReadTextFromFile(defaultFile);
+            return new DataFile<TData>(path, defaultFileText);
         }
     }
 }
--- InterSUCC/DataFile types/ReadOnlyDataFileGeneric.cs
+++ InterSUCC/DataFile types/ReadOnlyDataFileGeneric.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using SUCC;
 using SUCC.Abstractions;
+using SUCC.UnityStuff;
 using ClassImpl;
 
 namespace InterSUCC
@@ -10,6 +11,12 @@ namespace InterSUCC
         public TData Data { get; }
 
 
+        /// <summary>
+        /// Creates a new <see cref="ReadOnlyDataFile{TData}"/> object, using a text file in Unity's Resources folder for the default file text.
+        /// </summary>
+        /// <param name="path"> The path of the file. Can be either absolute or relative to the default path. </param>
+        /// <param name="defaultFile"> The path of the default file, relative to any Resources parent. </param>
+        /// <returns></returns>
         public ReadOnlyDataFile(string path, string defaultFileText = null) : base(path, defaultFileText)
         {
             this.Data = DataUtility<TData>.GenerateDataObject(this);
